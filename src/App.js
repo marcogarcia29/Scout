b42bc432fdac0d01338cdef7d777738fb6233b0f
portolucas <lucasportosd@gmail.com>
2021-12-13 15:45:09 -0300

    Revert "feature: payment"
    This reverts commit 826605cec34e847d3afb06b85d460e371de28be7.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useState } from "react";
-import "./App.css";
-import Teams from "./Components/Teams";
-import Navbar from "./Components/Navbar";
-import TeamContext from "./Components/TeamContext";
-import Home from "./Components/Home";
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { useState } from 'react';
+import './App.css';
+import Teams from './Components/Teams';
+import Navbar from './Components/Navbar';
+import TeamContext from './Components/TeamContext';
+import Home from './Components/Home';
 
 function App() {
   const [players, setPlayers] = useState([]);
@@ -12,15 +12,21 @@ function App() {
 
   return (
     <Router>
-      <TeamContext.Provider value={{ players, setPlayers, name, setName }}>
+      <TeamContext.Provider
+        value={{players, setPlayers, name, setName}}
+      >
         <div className="App">
-          <Navbar />
-          <div className="content">
-            <Routes>
-              <Route exact path="/" element={<Home />}></Route>
-              <Route path="/teams" element={<Teams />} />
-            </Routes>
-          </div>
+          <Navbar/>
+            <div className="content">
+              <Routes>
+                <Route exact path="/"
+                  element={<Home/>}>
+                </Route>
+                <Route path="/teams"
+                  element={<Teams/>}
+                />
+              </Routes>
+            </div>
         </div>
       </TeamContext.Provider>
     </Router>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,23 +1,17 @@
-import { Link } from "react-router-dom";
-import Teams from "./Teams";
+import { Link } from 'react-router-dom';
+import Teams from './Teams';
 
 const Navbar = () => {
-  return (
-    <div className="navbar">
-      <img src="https://img.icons8.com/office/40/000000/football.png" />
-      <ul className="menu">
-        <Link to="/">
-          <li>Home</li>
-        </Link>
-        <Link to="/teams">
-          <li>Criar times</li>
-        </Link>
-        <Link to="/">
-          <li>Login</li>
-        </Link>
-      </ul>
-    </div>
-  );
-};
+    return (
+        <div className="navbar">
+            <img src="https://img.icons8.com/office/40/000000/football.png"/>         
+            <ul className="menu">
+                <Link to="/"><li>Home</li></Link>
+                <Link to="/teams"><li>Criar times</li></Link>
+                <Link to="/"><li>Login</li></Link>
+            </ul>
+        </div>
+    )
+}
 
 export default Navbar;
