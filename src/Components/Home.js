826605cec34e847d3afb06b85d460e371de28be7
portolucas <lucasportosd@gmail.com>
2021-12-13 15:41:11 -0300

    feature: payment

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,35 +1,51 @@
-import React, {useContext, useEffect, useState} from "react";
+import React, { useContext, useEffect, useState } from "react";
 import TeamContext from "./TeamContext";
 
 const Home = () => {
-    const {players, setPlayers} = useContext(TeamContext);
-    const {name, setName} = useContext(TeamContext);
-    const addPlayer = (e) => {
-        e.preventDefault();
-        setPlayers([...players, name]);
-        setName("");
-    };
-    
-    return (
-        <>
-            <form className="testForm" onSubmit={addPlayer}>
-                <label>Nome do jogador </label>
-                <input type="text" value={name} onChange={(e) => setName(e.target.value)}/>
-                <button onClick={(e) => addPlayer(e)} type="button">
-                    Enviar
-                </button>
-            </form>
-            <div className="organized">
-                {players?.map(player =>
-                    <div key={player.id}>
-                        <p>{player}</p>
-                    </div>
-                )}
-            </div>
-        </>
+  const { players, setPlayers } = useContext(TeamContext);
+  const { name, setName } = useContext(TeamContext);
+  const addPlayer = (e) => {
+    e.preventDefault();
+    setPlayers([
+      ...players,
+      { name: name, id: players.length + 1, payment: false },
+    ]);
+    setName("");
+  };
 
+  const handleChange = (index) => {
+    const changePlayers = [...players];
+    changePlayers[index].payment = !changePlayers[index].payment;
+    setPlayers(changePlayers);
+  };
 
-    )
+  return (
+    <>
+      <form className="testForm" onSubmit={addPlayer}>
+        <label>Nome do jogador </label>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button onClick={(e) => addPlayer(e)} type="button">
+          Enviar
+        </button>
+      </form>
+      <div className="organized">
+        {players?.map((player, index) => (
+          <div key={player.id}>
+            <p>{player.name}</p>
+            <input
+              type="checkbox"
+              checked={player.payment}
+              onChange={() => handleChange(index)}
+            />
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default Home;
--- src/Components/Teams.js
+++ src/Components/Teams.js
@@ -1,54 +1,57 @@
-import React, { useState, useContext} from "react";
+import React, { useState, useContext } from "react";
 import TeamContext from "./TeamContext";
 
 const Teams = () => {
-    const {players, setPlayers} = useContext(TeamContext);
-    const [team1, setTeam1] = useState([]);
-    const [team2, setTeam2] = useState([]);
+  const { players, setPlayers } = useContext(TeamContext);
+  const [team1, setTeam1] = useState([]);
+  const [team2, setTeam2] = useState([]);
 
-    const handlePlayers = () => {
-        const playerName = players[Math.floor(Math.random()*players.length)];
-        setTeam1([...team1, playerName]);
-        const getPlayer = players.indexOf(playerName);
-        players.splice(getPlayer, 1);
-    };
+  const handlePlayers = () => {
+    const playerName = players[Math.floor(Math.random() * players.length)];
+    setTeam1([...team1, playerName]);
+    const getPlayer = players.indexOf(playerName);
+    players.splice(getPlayer, 1);
+  };
 
-    const handlePlayers2 = () => {
-        const playerName = players[Math.floor(Math.random()*players.length)];
-        setTeam2([...team2, playerName]);
-        const getPlayer = players.indexOf(playerName);
-        players.splice(getPlayer, 1);
-    };
+  const handlePlayers2 = () => {
+    const playerName = players[Math.floor(Math.random() * players.length)];
+    setTeam2([...team2, playerName]);
+    const getPlayer = players.indexOf(playerName);
+    players.splice(getPlayer, 1);
+  };
 
-    return (
-        <div className="teamSelector">
-            <div className="organized">
-                {players?.map(player =>
-                    <div key={player.id}>
-                        <p>{player}</p>
-                    </div>
-                )}
-                <button onClick={() => handlePlayers()}>SELECIONAR JOGADOR TIME 1</button>
-                <button onClick={() => handlePlayers2()}>SELECIONAR JOGADOR TIME 2</button>
-                
-            </div>
-            <div className="teams">
-                <div className="alignTeams">
-                <h3>TIME A</h3>
-                    {team1?.map(selected => 
-                        <p>{selected}</p>
-                    )}
-                </div>
-                <div className="alignTeams">
-                    <h3>TIME B</h3>
-                    {team2?.map(selected => 
-                        <p>{selected}</p>
-                    )}
-                </div>
-            </div>
-            {console.log(players)}
+  return (
+    <div className="teamSelector">
+      <div className="organized">
+        {players?.map((player) => (
+          <div key={player.id}>
+            <p>{player}</p>
+          </div>
+        ))}
+        <button onClick={() => handlePlayers()}>
+          SELECIONAR JOGADOR TIME 1
+        </button>
+        <button onClick={() => handlePlayers2()}>
+          SELECIONAR JOGADOR TIME 2
+        </button>
+      </div>
+      <div className="teams">
+        <div className="alignTeams">
+          <h3>TIME A</h3>
+          {team1?.map((selected) => (
+            <p>{selected}</p>
+          ))}
         </div>
-    );
+        <div className="alignTeams">
+          <h3>TIME B</h3>
+          {team2?.map((selected) => (
+            <p>{selected}</p>
+          ))}
+        </div>
+      </div>
+      {console.log(players)}
+    </div>
+  );
 };
 
 export default Teams;
