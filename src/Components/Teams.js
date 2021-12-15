c48add1807d224088a23f4353dbf2968e192198a
Framework <marco.silva.garcia2901@gmail.com>
2021-12-15 11:26:10 -0300

    fix: bug with player add button

--- src/Components/Teams.js
+++ src/Components/Teams.js
@@ -7,45 +7,38 @@ const Teams = () => {
   const {team2, setTeam2} = useContext(TeamContext);
 
   const handlePlayers = (e) => {
-    e.preventDefault();
-    const playerName = players[Math.floor(Math.random() * players.length)];
-    setTeam1([...team1, playerName]);
-    const getPlayer = players.indexOf(playerName);
-    players.splice(getPlayer, 1);
+    if (players.length) {
+      const playerName = players[Math.floor(Math.random() * players.length)];
+      setTeam1([...team1, playerName]);
+      const getPlayer = players.indexOf(playerName);
+      players.splice(getPlayer, 1);
+    }
+    
   };
 
   const handlePlayers2 = () => {
-    const playerName = players[Math.floor(Math.random() * players.length)];
-    setTeam2([...team2, playerName]);
-    const getPlayer = players.indexOf(playerName);
-    players.splice(getPlayer, 1);
+    if (players.length) {
+      const playerName = players[Math.floor(Math.random() * players.length)];
+      setTeam2([...team2, playerName]);
+      const getPlayer = players.indexOf(playerName);
+      players.splice(getPlayer, 1);
+    }
   };
 
   return (
     <div className="teamSelector">
       <div className="organized">
+        <h2>JOGADORES</h2>
         {players?.map((player) => (
           <div key={player.id}>
-<<<<<<< HEAD
-            <h2>JOGADORES</h2>
             <p>{player.name}</p>
           </div>
         ))}
         <button className="btnTeam" onClick={(e) => handlePlayers(e)}>
           SELECIONAR JOGADOR TIME A
         </button>
-        <button className="btnTeam" onClick={() => handlePlayers2()}>
+        <button className="btnTeam" onClick={(e) => handlePlayers2(e)}>
           SELECIONAR JOGADOR TIME B
-=======
-            <p>{player.name}</p>
-          </div>
-        ))}
-        <button onClick={(e) => handlePlayers(e)}>
-          SELECIONAR JOGADOR TIME 1
-        </button>
-        <button onClick={() => handlePlayers2()}>
-          SELECIONAR JOGADOR TIME 2
->>>>>>> b0c0f85ab97325ae098e76df6ca2184de6dd7fe9
         </button>
       </div>
       <div className="teams">
