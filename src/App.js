583781cdc410d004719ae72bf3c2dbe8261b0881
Jonatas de Oliveira Coelho <jonatas.oliveiracoelho@gmail.com>
2021-12-16 14:51:50 -0300

    refactor: refactoring react routers. fix lint

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { useState } from 'react'
-import './App.css'
-import Teams from './Components/Teams'
-import Navbar from './Components/Navbar'
-import TeamContext from './Components/TeamContext'
-import Home from './Components/Home'
-import { Scout } from './Components/Scout'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import "./App.css";
+import Teams from "./Components/Teams";
+import Navbar from "./Components/Navbar";
+import TeamContext from "./Components/TeamContext";
+import Home from "./Components/Home";
+import { Scout } from "./Components/Scout";
 
 function App() {
-  const [players, setPlayers] = useState([])
-  const [name, setName] = useState('')
-  const [team1, setTeam1] = useState([])
-  const [team2, setTeam2] = useState([])
+  const [players, setPlayers] = useState([]);
+  const [name, setName] = useState("");
+  const [team1, setTeam1] = useState([]);
+  const [team2, setTeam2] = useState([]);
 
   return (
     <Router>
@@ -24,22 +24,22 @@ function App() {
           team1,
           setTeam1,
           team2,
-          setTeam2
+          setTeam2,
         }}
       >
         <div className="App">
           <Navbar />
           <div className="content">
             <Routes>
-              <Route exact path="/" element={<Home />}></Route>
-              <Route path="/teams" element={<Teams />} />
-              <Route path="/scout" element={<Scout />} />
+              <Route exact path="/" component={Home} />
+              <Route path="/teams" component={Teams} />
+              <Route path="/scout" component={Scout} />
             </Routes>
           </div>
         </div>
       </TeamContext.Provider>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
