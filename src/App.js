d2f486da3582e17ecad7267cdd93e3731039c6c0
Jonatas de Oliveira Coelho <jonatas.oliveiracoelho@gmail.com>
2021-12-17 11:11:32 -0300

    fix: back routes to element props

--- src/App.js
+++ src/App.js
@@ -37,8 +37,8 @@ function App() {
           <div className="content">
             <Routes>
               <Route exact path="/" element={<Home />} />
-              <Route path="/teams" component={Teams} />
-              <Route path="/scout" component={Scout} />
+              <Route path="/teams" element={<Teams />} />
+              <Route path="/scout" element={<Scout />} />
             </Routes>
           </div>
         </div>
